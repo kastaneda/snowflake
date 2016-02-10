554db95e67c377aed44db70a1d2aaf0e6fd4006e
Dmitry Kolesnikov <kastaneda@gmail.com>
2016-02-11 01:32:15 +0200

    some basic Silex code

--- web/index.php
+++ web/index.php
@@ -1,2 +1,14 @@
 <?php
-phpinfo();
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->get('/', function () use ($app) {
+    return $app->redirect('/hello/world');
+});
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return '<h1>Hello, ' . $app->escape(ucfirst($name)) . '!</h1>';
+});
+
+$app->run();
